792e86a1d61acd42cdd84f924f909cb126f9781f
VasekD <doskar@spsjc.cz>
2026-01-13 15:28:23 +0100

    Edits2

--- SeznamLidi/MainWindow.xaml.cs
+++ SeznamLidi/MainWindow.xaml.cs
@@ -3,6 +3,7 @@ using SeznamLidi.Interfaces;
 using SeznamLidi.Managers;
 using SeznamLidi.Models;
 using SeznamLidi.Repositories;
+using SeznamLidi.Windows;
 using System.Collections.ObjectModel;
 using System.Windows;
 
@@ -13,17 +14,26 @@ namespace SeznamLidi
     /// </summary>
     public partial class MainWindow : Window
     {
+        public ObservableCollection<Person> Data { get; set; }
         public IPersonManager PersonManager {  get; set; }
         public MainWindow()
         {
             IPersonRepository personRepository = new PersonRepository(new PersonContext());
             PersonManager = new PersonManager(personRepository);
-            InitializeComponent();
-            LV.DataContext = new ObservableCollection<Person>(PersonManager.GetAll());
+            Data = new ObservableCollection<Person>(PersonManager.GetAll());
+            InitializeComponent();            
+            LV.DataContext = Data;
         }
 
 
         void OnClick(object sender, RoutedEventArgs e) { }
+
+        void AddClick(object sender, RoutedEventArgs e) 
+        {
+            CreateNewPersonWindow CNPW = new(PersonManager);
+            CNPW.Closed += (s, e) => { Data.Add(CNPW.newPerson); };
+            CNPW.ShowDialog();
+        }
     }
 
     // TODO: CRUD, DI
---
+++ SeznamLidi/Windows/CreateNewPersonWindow.xaml.cs
@@ -0,0 +1,49 @@
+﻿using SeznamLidi.Interfaces;
+using SeznamLidi.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace SeznamLidi.Windows
+{
+    /// <summary>
+    /// Interaction logic for CreateNewPersonWindow.xaml
+    /// </summary>
+    public partial class CreateNewPersonWindow : Window
+    {
+        IPersonManager _manager;
+        public Person newPerson { get; private set; }
+        public CreateNewPersonWindow(IPersonManager manager)
+        {
+            _manager = manager;
+            InitializeComponent();
+        }
+
+        private void SaveButton_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                string firstName = FN.Text;
+                string lastName = LN.Text;
+                int age = int.Parse(Age.Text);
+                newPerson = new Person { Age = age ,FirstName=firstName,LastName=lastName};
+                _manager.Add(newPerson);
+                this.Close();
+            }
+            catch (FormatException ex)
+            {
+                MessageBox.Show(ex.Message, "Error", MessageBoxButton.OK, MessageBoxImage.Error);
+            }
+        }
+    }
+}
